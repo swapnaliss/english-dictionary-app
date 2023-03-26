6d15f06c3b17fd68377c45b7b9d9b605b76bafd8
swapnaliss <swapnalisbhargude123@gmail.com>
2023-03-27 01:30:02 +0530

    functionality done for information text

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const inputE1 = document.getElementById("input");
+const infoTextEl = document.getElementById("info-text");
 
 const fetchAPI = async (word) => {
     console.log(word);
     try {
+        infoTextEl.style.display = "block";
+        infoTextEl.innerText = `Searching the meaning of "${word}"`;
         const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
         const result = await fetch(url).then((response) => response.json());
-
+             
+        infoTextEl.style.display = "none";
+         
     } catch (error) {
         console.log(error);
     }
