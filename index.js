906c5e2399212febd7f0cca11d1d9f55195d9c64
swapnaliss <swapnalisbhargude123@gmail.com>
2023-03-27 01:42:47 +0530

    displaying title of searched word

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const inputE1 = document.getElementById("input");
 const infoTextEl = document.getElementById("info-text");
 const meaningContainerEl = document.getElementById("meaning-container");
+const titleEl = document.getElementById("title");
 
 const fetchAPI = async (word) => {
     console.log(word);
     try {
         infoTextEl.style.display = "block";
-        meaningContainerEl.style.display = "none";
+       
         infoTextEl.innerText = `Searching the meaning of "${word}"`;
+
         const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
         const result = await fetch(url).then((response) => response.json());
-             
+
         infoTextEl.style.display = "none";
         meaningContainerEl.style.display = "block";
-         
+        titleEl.innerText = result[0].word;
+        
     } catch (error) {
         console.log(error);
     }
