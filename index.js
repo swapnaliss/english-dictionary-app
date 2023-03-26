56603fde761dfa014b01bef00ecd1451ca849b84
swapnaliss <swapnalisbhargude123@gmail.com>
2023-03-27 01:36:58 +0530

    displaying meaning container

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const inputE1 = document.getElementById("input");
 const infoTextEl = document.getElementById("info-text");
+const meaningContainerEl = document.getElementById("meaning-container");
 
 const fetchAPI = async (word) => {
     console.log(word);
     try {
         infoTextEl.style.display = "block";
+        meaningContainerEl.style.display = "none";
         infoTextEl.innerText = `Searching the meaning of "${word}"`;
         const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
         const result = await fetch(url).then((response) => response.json());
              
         infoTextEl.style.display = "none";
+        meaningContainerEl.style.display = "block";
          
     } catch (error) {
         console.log(error);
